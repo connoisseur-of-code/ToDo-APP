935287aa4db1d20829ad08bc976eb4dbe76d7df6
connoisseur-of-code <code.nlesiecki@skiff.com>
2024-01-28 15:29:26 +0100

    Upload files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Hello, React!</h1>
+			</header>
+		</div>
+	);
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,4 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{js,ts,jsx,tsx}', './public/index.html'],
+};
